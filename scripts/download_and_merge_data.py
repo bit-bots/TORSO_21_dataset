d0b3a6c540d3632c675eef7de6477369e55dc82f
Timon Engelke <timon.engelke@online.de>
2021-03-25 00:19:20 +0100

    Add imagetagger download script

--- scripts/download_and_merge_data.py
+++ scripts/download_and_merge_data.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python3
+import getpass
 import os
+import sys
 import yaml
 from download import login, download_zip, download_annotations
 
 SETS = [261, 156, 614]
 EXPORT_FORMAT = 196
 OUTPUT_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data_raw'))
+SET_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '%s'))
 
 if __name__ == '__main__':
     print(f'The merged files will be saved to {OUTPUT_FOLDER}')
 
-    login()
+    username = input('ImageTagger username: ')
+    password = getpass.getpass()
+    login_data = login(username, password)
+    if not login_data:
+        sys.exit('Login failed')
+
     os.makedirs(OUTPUT_FOLDER, exist_ok=True)
     annotation_file = os.path.join(OUTPUT_FOLDER, 'annotations.yaml')
 
     for s in SETS:
-        print(f'Downloading imageset {s}')
-        download_zip(s)
-
-        print(f'Downloading annotations for set {s}')
-        download_annotations(s, EXPORT_FORMAT)
+        set_folder = SET_FOLDER % s
+        print(f'Downloading imageset {s} to {set_folder}')
+        download_zip(s, set_folder, login_data)
 
         print(f'Moving images of set {s}')
         for image in os.listdir(str(s)):
             os.rename(os.path.join(str(s), image), os.path.join(OUTPUT_FOLDER, f'{s}-{image}'))
         
+        print(f'Downloading annotations for set {s}')
+        download_annotations(s, EXPORT_FORMAT, set_folder, login_data)
+
         print(f'Converting annotations for set {s}')
         # Read annotations for this set
         with open(f'{s}/{s}.txt') as f:
