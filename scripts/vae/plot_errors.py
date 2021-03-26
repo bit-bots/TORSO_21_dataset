45bb3bdf5509d317c3fddb566d3d6a6e01a15de6
Florian <florian@flova.de>
2021-03-26 15:02:52 +0100

    Add Labels

--- scripts/vae/plot_errors.py
+++ scripts/vae/plot_errors.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 with open("embeddings.pickle", "rb") as f:
     data = pickle.load(f)
 
-plt.hist(data["errors"], density=True, bins=30)
+plt.hist(data["errors"], density=False, bins=30)
+plt.ylabel('Number of images')
+plt.xlabel('Reconstruction error')
 
 plt.show() 
