cecaf517a5514a45626a66b4e1b19a72cf77e8dc
Florian Vahl <7vahl@informatik.uni-hamburg.de>
2021-04-08 00:17:18 +0200

    New fancy plot

--- scripts/vae/plot_errors.py
+++ scripts/vae/plot_errors.py
@@ -7,10 +7,18 @@ with open("embeddings.pickle", "rb") as f:
 
 sns.set_style("darkgrid")
 
-plot = sns.distplot(data["errors"], density=False, bins=30)
+N, bins, patches = plt.hist(data["errors"], bins=30)
 
-plt.axvline(100, 0, data["errors"].mean() + data["errors"].std() * 1.64)
+colors    = ["#7995c4b0", "#ff0000b0"]
+divisions = [range(12), range(12, 30)]
+labels    = ["Successful Reconstruction", "Failed Reconstruction"]
 
-plot.set(xlabel='Reconstruction error', ylabel='Number of images')
+for d in divisions:
+    patches[list(d)[0]].set_label(labels[divisions.index(d)])
+    for i in d:
+        patches[i].set_color(colors[divisions.index(d)])
 
+plt.xlabel("Reconstruction error")
+plt.ylabel("Number of images")
+plt.legend()
 plt.show() 
