52f31705341312cbb738a670877718c6432fd984
Florian <florian@flova.de>
2021-04-07 23:21:02 +0200

    Use seaborn for error plot

--- scripts/vae/plot_errors.py
+++ scripts/vae/plot_errors.py
@@ -1,11 +1,16 @@
 import pickle
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 with open("embeddings.pickle", "rb") as f:
     data = pickle.load(f)
 
-plt.hist(data["errors"], density=False, bins=30)
-plt.ylabel('Number of images')
-plt.xlabel('Reconstruction error')
+sns.set_style("darkgrid")
+
+plot = sns.distplot(data["errors"], density=False, bins=30)
+
+plt.axvline(100, 0, data["errors"].mean() + data["errors"].std() * 1.64)
+
+plot.set(xlabel='Reconstruction error', ylabel='Number of images')
 
 plt.show() 
