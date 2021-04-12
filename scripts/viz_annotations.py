058ebf71a55ba56ba4b66254e549e9946df627c7
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-04-12 20:15:25 +0200

    improve viz annotation script

--- scripts/viz_annotations.py
+++ scripts/viz_annotations.py
@@ -2,40 +2,50 @@ import yaml
 import cv2
 import numpy as np
 import os
+import pickle
 
 MAIN_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 ANNOTATION_INPUT_FILE = os.path.join(MAIN_FOLDER, 'data/annotations_with_metadata.yaml')
-with open(ANNOTATION_INPUT_FILE) as f:
-    annos = yaml.load(f, Loader=yaml.Loader)["images"]
-
+ANNOTATION_INPUT_FILE_PICKLED = os.path.join(MAIN_FOLDER, 'data/annotations_with_metadata.pkl')
+with open(ANNOTATION_INPUT_FILE_PICKLED, "rb") as f:
+    annos = pickle.load(f)["images"]
+    print("Press 'a' and 'd' to move between images. 'A' and 'D' let you jump 100 images. 'q' closes. 'n' toggles not in image text")
     files = list(annos)
     files.sort()
-
-    for f in files:
+    not_in_image = True
+    i = 0
+    while True:
+        f = files[i]
         img = cv2.imread(f)
-        y = 0
+        y = 20
         for a in annos[f]["annotations"]:
             if not a["in_image"]:
-                cv2.putText(img, f"{a['type']} not in image", (0, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-                y += 20
+                if not_in_image:
+                    cv2.putText(img, f"{a['type']} not in image", (0, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                    y += 20
             else:
                 if a["type"] == "robot":
                     color = (255,0,0)
                 elif a["type"] == "ball":
                     color = (0,255,0)
+                elif a["type"] == "goalpost":
+                    color = (255,0,255)
                 elif a["type"] == "left_goalpost":
                     color = (255,0,255)
                 elif a["type"] == "right_goalpost":
                     color = (0,255,255)
                 elif a["type"] == "top_bar":
                     color = (0,0,255)
+                elif a["type"] == "field edge":
+                    color = (255,255,0)
+                elif a["type"] == "obstacle":
+                    color = (0,0,0)
 
-                if a["type"] == "robot" or a["type"] == "ball": # bounding boxes
+                if a["type"] == "robot" or a["type"] == "ball" or a["type"] == "obstacle": # bounding boxes
                     x_start = int(a["vector"][0][0])
                     x_stop = int(a["vector"][1][0])
                     y_start = int(a["vector"][0][1])
                     y_stop = int(a["vector"][1][1])
-                    print(f"x_start: {x_start}, x_stop: {x_stop}, y_start: {y_start}, y_stop: {y_stop}")
                     contours = np.ndarray((4,2), dtype=int)
                     contours[0][0] = x_start
                     contours[0][1] = y_start
@@ -45,6 +55,7 @@ with open(ANNOTATION_INPUT_FILE) as f:
                     contours[2][1] = y_stop
                     contours[3][0] = x_stop
                     contours[3][1] = y_start
+                    cv2.drawContours(img, [contours], -1, color, 5)
                 elif a["type"] == "goalpost" or a["type"] == "left_goalpost" or a["type"] == "right_goalpost" or a["type"] == "top_bar":
                     contours = np.ndarray((4, 2), dtype=int)
                     contours[0][0] = int(a["vector"][0][0])
@@ -55,13 +66,45 @@ with open(ANNOTATION_INPUT_FILE) as f:
                     contours[2][1] = int(a["vector"][2][1])
                     contours[3][0] = int(a["vector"][3][0])
                     contours[3][1] = int(a["vector"][3][1])
+                    cv2.drawContours(img, [contours], -1, color, 5)
+                elif a["type"] == "field edge":
+                    points = []
+                    for point in a["vector"]:
+                        points.append(point)
+                    pts = np.array(points, np.int32)
+                    pts = pts.reshape((-1, 1, 2))
+                    img = cv2.polylines(img, [pts], False, color, 5)
                 else:
-                    continue#todo intersections not handled
-
-                cv2.drawContours(img, [contours], -1, color, 5)
+                    color = (0,0,0)
+                    if a["type"] == "L-Intersection":
+                        txt = "L"
+                    elif a["type"] == "T-Intersection":
+                        txt = "T"
+                    elif a["type"] == "X-Intersection":
+                        txt = "X"
+                    else:
+                        print(a["type"])
+                        exit(1)
+                    
+                    txt_size = cv2.getTextSize(txt, cv2.FONT_HERSHEY_COMPLEX, 1, 2)
+                    cv2.putText(img, txt, (int(a["vector"][0][0]-(txt_size[0][0]/2)), int(a["vector"][0][1]+(txt_size[0][1]/2))), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
 
 
         cv2.imshow("img", img)
-        cv2.waitKey(0)
+        key = cv2.waitKey(0)
+        if key in [83, 100] : #d
+            i += 1
+        elif key == 68: #D
+            i += 100
+        elif key in [81, 97]: #a
+            i -= 1
+        elif key == 65: #A
+            i -= 100
+        elif key in [27, 113]:
+            exit(0)
+        elif key == 110:
+            not_in_image = not not_in_image
+        i = max(0, i)
+        i = min(len(files), i)
 
     cv2.destroyAllWindows()
