92cfd22b937a132ca367a569a7df4b2bf4c00232
Jasper <31923619+jgueldenstein@users.noreply.github.com>
2021-04-12 17:37:44 +0200

    Update viz_annotations.py

--- scripts/viz_annotations.py
+++ scripts/viz_annotations.py
@@ -16,21 +16,48 @@ for f in files:
         if not a["in_image"]:
             cv2.putText(img, f"{a['type']} not in image", (0, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
             y += 20
-        elif a["type"] == "robot":
-            x_start = int(a["vector"][0][0])
-            x_stop = int(a["vector"][1][0])
-            y_start = int(a["vector"][1][0])
-            y_stop = int(a["vector"][1][1])
-            contours = np.array([(x_start, y_start), (x_start,y_stop), (x_stop, y_start), (x_stop, y_stop)])
-            cv2.drawContours(img, contours, -1, (255,0,0), 5)
+        else:
+            if a["type"] == "robot":
+                color = (255,0,0)
+            elif a["type"] == "ball":
+                color = (0,255,0)
+            elif a["type"] == "left_goalpost":
+                color = (255,0,255)
+            elif a["type"] == "right_goalpost":
+                color = (0,255,255)
+            elif a["type"] == "top_bar":
+                color = (0,0,255)
+
+            if a["type"] == "robot" or a["type"] == "ball": # bounding boxes
+                x_start = int(a["vector"][0][0])
+                x_stop = int(a["vector"][1][0])
+                y_start = int(a["vector"][0][1])
+                y_stop = int(a["vector"][1][1])
+                print(f"x_start: {x_start}, x_stop: {x_stop}, y_start: {y_start}, y_stop: {y_stop}")
+                contours = np.ndarray((4,2), dtype=int)
+                contours[0][0] = x_start
+                contours[0][1] = y_start
+                contours[1][0] = x_start
+                contours[1][1] = y_stop
+                contours[2][0] = x_stop
+                contours[2][1] = y_stop
+                contours[3][0] = x_stop
+                contours[3][1] = y_start
+            elif a["type"] == "left_goalpost" or a["type"] == "right_goalpost" or a["type"] == "top_bar":
+                contours = np.ndarray((4, 2), dtype=int)
+                contours[0][0] = int(a["vector"][0][0])
+                contours[0][1] = int(a["vector"][0][1])
+                contours[1][0] = int(a["vector"][1][0])
+                contours[1][1] = int(a["vector"][1][1])
+                contours[2][0] = int(a["vector"][2][0])
+                contours[2][1] = int(a["vector"][2][1])
+                contours[3][0] = int(a["vector"][3][0])
+                contours[3][1] = int(a["vector"][3][1])
+
+            cv2.drawContours(img, [contours], -1, color, 5)
+
 
     cv2.imshow("img", img)
     cv2.waitKey(0)
 
 cv2.destroyAllWindows()
-
-
-#debug = cv2.drawContours(cv2.UMat(img), [box.astype(int)], -1, (255, 255, 255), 10)
-#cv2.imshow(key, debug)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
